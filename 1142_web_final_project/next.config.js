212241412253c2e9b351f2f2c7d28838f61cdcf2
neboleic <kate13yyll@gmail.com>
2026-06-10 22:45:43 +0800

    fix game1 pic

--- 1142_web_final_project/next.config.js
+++ 1142_web_final_project/next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',   // 靜態 HTML 輸出，Vercel 零設定部署
-  trailingSlash: true,
-  images: { unoptimized: true },
-};
+const nextConfig = {}
 
-module.exports = nextConfig;
+module.exports = nextConfig
